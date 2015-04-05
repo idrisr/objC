62ac81e4c0da21e81a5455d36c6054eed1e824bf
Idris Raja <idris.raja@gmail.com>
2015-04-05 14:28:09 +0700

    init commits

---
+++ Turkey/Turkey/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Turkey
+//
+//  Created by idris on 4/5/15.
+//  Copyright (c) 2015 Idris Raja. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+
+
+    return 0;
+}
