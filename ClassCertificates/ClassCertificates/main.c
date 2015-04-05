642d61c598ba72e342149bfabab6298738d2dbac
Idris Raja <idris.raja@gmail.com>
2015-04-05 14:49:17 +0700

    what a sad way to do it

--- ClassCertificates/ClassCertificates/main.c
+++ ClassCertificates/ClassCertificates/main.c
@@ -9,5 +9,10 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
+    printf("Kate has done as much Cocoa Programming as I could fit into 5 day.\n");
+    printf("Bo has done as much Objective-C Programming as I could fit into 2 days.\n");
+    printf("Mike has done as much Python programming as I could fit into 5 days.\n");
+    printf("Liz has done as much iOS Programming as I could fit into 5 days.\n");
+
     return 0;
 }
