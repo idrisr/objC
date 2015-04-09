1ca52ce021e697970145364da8c3eedcf250ddac
Idris Raja <idris.raja@gmail.com>
2015-04-10 00:40:45 +0700

    challenge to find list overlap

--- GroceryList/GroceryList/main.m
+++ GroceryList/GroceryList/main.m
@@ -10,16 +10,30 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSMutableArray *groceries = [NSMutableArray array];
-        NSString *item1 = @"apple";
-        NSString *item2 = @"orange";
-        NSString *item3 = @"weed";
-        NSString *item4 = @"kale kombucha";
-        groceries = @[item1, item2, item3, item4];
+        // read in a file as a huge string, ignoring possibility of error
+        NSString *nameString = [NSString stringWithContentsOfFile:@"/usr/share/dict/propernames"
+                                                         encoding:NSUTF8StringEncoding
+                                                            error:NULL];
 
-        for (NSString *grocery in groceries){
-            NSLog(@"%@", grocery);
+        NSString *wordString = [NSString stringWithContentsOfFile:@"/usr/share/dict/words"
+                                                         encoding:NSUTF8StringEncoding
+                                                            error:NULL];
+
+
+        // break it into an array of strings
+        NSArray *names = [nameString componentsSeparatedByString:@"\n"];
+        NSArray *words = [wordString componentsSeparatedByString:@"\n"];
+
+        // Go through the array one string at a time
+        int count = 0;
+        for (NSString *name in names){
+            if ([words containsObject:name] || [words containsObject:[name lowercaseString]]){
+                // NSLog(@"Match found: %@", name);
+                count++;
+            }
         }
+        NSLog(@"Matches found %i", count);
+
     }
     return 0;
 }
