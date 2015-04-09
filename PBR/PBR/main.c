d3d1105e40d921e3b22deee071c59f147538dea1
Idris Raja <idris.raja@gmail.com>
2015-04-09 14:27:56 +0700

    example of pass by reference

--- PBR/PBR/main.c
+++ PBR/PBR/main.c
@@ -9,15 +9,27 @@
 #include <stdio.h>
 #include <math.h>
 
+void metersToFeetandInches(double meters, unsigned int *ftPtr, double *inPtr){
+    double rawFeet = meters * 3.281;
+
+    unsigned int feet = (unsigned int)floor(rawFeet);
+    printf("Storing %u to the address %p\n", feet, ftPtr);
+    *ftPtr = feet;
+
+    // calculate inches
+    double fractionalFoot = rawFeet - feet;
+    double inches = fractionalFoot * 12.0;
+
+    // store the number of inches at the supplied address
+    printf("Storing %.2f to the address %p\n", inches, inPtr);
+    *inPtr = inches;
+}
+
 int main(int argc, const char * argv[]) {
-    double pi = 3.14;
-    double integerPart;
-    double fractionalPart;
+    double meters = 3.0;
+    unsigned int feet;
+    double inches;
 
-    // Pass the address of integerPart as an argument
-    fractionalPart = modf(pi, &integerPart);
-
-    // Find the value stored in integerPart
-    printf("integerPart = %.0f, fractionalPart = %.2f\n", integerPart, fractionalPart);
+    metersToFeetandInches(meters, &feet, &inches);
     return 0;
 }
