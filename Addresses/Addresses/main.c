15bb361be197d3a7bd3a612ed9af94402f54c1fa
Idris Raja <idris.raja@gmail.com>
2015-04-09 13:29:25 +0700

    use NULL to assign memory for a pointer but to keep it currently empty

--- Addresses/Addresses/main.c
+++ Addresses/Addresses/main.c
@@ -9,14 +9,14 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    int i = 17;
-    int *addressOfI = &i;
-    printf("i stores its value at %p\n", addressOfI);
-    *addressOfI = 89;
-    printf("Now i is %d\n", i);
-
-    printf("An int is %zu bytes\n", sizeof(int));
-    printf("A pointer is %zu bytes\n", sizeof(int *));
+    float *myPointer;
+    // set myPointer to NULL for now, I'll store an address there eventually
+    myPointer = NULL;
+    if (myPointer){
+        printf("myPointer is set\n");
+    } else {
+        printf("myPointer is not set\n");
+    }
 
     return 0;
 }
