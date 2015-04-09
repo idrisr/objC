9a7341c4a9b1e52a5c0c374397d578bd52142130
Idris Raja <idris.raja@gmail.com>
2015-04-09 13:23:18 +0700

    you can also use the * operator on the left-hand side of an assignment to store data at a particular address

--- Addresses/Addresses/main.c
+++ Addresses/Addresses/main.c
@@ -11,9 +11,8 @@
 int main(int argc, const char * argv[]) {
     int i = 17;
     int *addressOfI = &i;
-
     printf("i stores its value at %p\n", addressOfI);
-    printf("This function starts at %p\n", main);
-    printf("the int stored at addressOfI is %d\n", *addressOfI);
+    *addressOfI = 89;
+    printf("Now i is %d\n", i);
     return 0;
 }
