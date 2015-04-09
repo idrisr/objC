5a8a9e074e1f3c5aba2f143af94f0bbba0f8bf6a
Idris Raja <idris.raja@gmail.com>
2015-04-09 16:54:06 +0700

    and now the pointer on the stack points to an object on the heap

--- TimesTwo/TimesTwo/main.m
+++ TimesTwo/TimesTwo/main.m
@@ -10,7 +10,7 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSDate *currentTime = nil;
+        NSDate *currentTime = [NSDate date];
         NSLog(@"currentTime's value is %p", currentTime);
     }
     return 0;
