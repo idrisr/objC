30d92141e822a1402db2e070862140db11b4a07f
Idris Raja <idris.raja@gmail.com>
2015-04-09 16:53:25 +0700

    currently a NSDate pointer on the stack that points to nothing on the heap

---
+++ TimesTwo/TimesTwo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TimesTwo
+//
+//  Created by idris on 4/9/15.
+//  Copyright (c) 2015 Idris Raja. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSDate *currentTime = nil;
+        NSLog(@"currentTime's value is %p", currentTime);
+    }
+    return 0;
+}
