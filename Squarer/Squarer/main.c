fd64e05b68daa91eddb1b906e0a5dd629c85b4be
Idris Raja <idris.raja@gmail.com>
2015-04-09 12:37:14 +0700

    simple challenge

---
+++ Squarer/Squarer/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Squarer
+//
+//  Created by idris on 4/9/15.
+//  Copyright (c) 2015 Idris Raja. All rights reserved.
+//
+
+#include <stdio.h>
+
+void squarer(int n){
+    printf("\"%i\" squared is \"25\"\n", n);
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+   squarer(5);
+}
