68cf34fecb645d0c2c29f4dee0b372229998e8f3
Idris Raja <idris.raja@gmail.com>
2015-04-09 12:54:38 +0700

    done with ch 7

---
+++ Numbers/Numbers/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Numbers
+//
+//  Created by idris on 4/9/15.
+//  Copyright (c) 2015 Idris Raja. All rights reserved.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char * argv[]) {
+    int n = 1;
+    double x = sin(n);
+    printf("The sine of 1 is: %.3f\n", x);
+}
