9a5343a66ae894968bfd5227a57cae3a24737efa
Idris Raja <idris.raja@gmail.com>
2015-04-10 21:17:48 +0700

    In a parent-child relationship, the general rule for preventing this type of strong reference cycle is the parent owns the child, but the child should not own the parent.

--- BMITime/BNRAsset.h
+++ BMITime/BNRAsset.h
@@ -11,6 +11,6 @@
 
 @interface BNRAsset : NSObject
 @property (nonatomic, copy) NSString *label;
-@property (nonatomic) BNREmployee *holder;
+@property (nonatomic, weak) BNREmployee *holder;
 @property (nonatomic) unsigned int resaleValue;
 @end
