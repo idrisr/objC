8957894e142c312278ce0c33881f6ef5a7ad85ec
Idris Raja <idris.raja@gmail.com>
2015-04-10 12:07:44 +0700

    do some inheritrance shit

--- BMITime/BMITime/main.m
+++ BMITime/BMITime/main.m
@@ -19,14 +19,19 @@ int main(int argc, const char * argv[]) {
         mikey.weightInKilos = 96;
         mikey.heightInMeters = 1.8;
 
+        mikey.employeeID = 12;
+        mikey.hireDate = [NSDate dateWithNaturalLanguageString:@"Aug 2nd, 2010"];
+
         // Log the instance variables using the getters
         float height = mikey.heightInMeters;
         int weight = mikey.weightInKilos;
         NSLog(@"mikey is %.2f meters tall and weighs %d kilograms", height, weight);
+        NSLog(@"Employee %u hired on %@", mikey.employeeID, mikey.hireDate);
 
         // Log some values using custom methods
         float bmi = mikey.bodyMassIndex;
-        NSLog(@"mikey has a BMI of %f", bmi);
+        double years = [mikey yearsOfEmployment];
+        NSLog(@"mikey has a BMI of %f, has worked with us for %.2f years", bmi, years);
     }
     return 0;
 }
