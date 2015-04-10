c9b7714e9187e16eb36912c8898e7b08d6a5067c
Idris Raja <idris.raja@gmail.com>
2015-04-10 19:32:07 +0700

    note that this doesnt work. need to create the array the first time around

---
+++ Stocks/BNRStockPortfolio.h
@@ -0,0 +1,13 @@
+//
+//  BNRStockPortfolio.h
+//  Stocks
+//
+//  Created by idris on 4/10/15.
+//  Copyright (c) 2015 Idris Raja. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRStockPortfolio : NSObject
+
+@end
---
+++ Stocks/BNRStockPortfolio.m
@@ -0,0 +1,13 @@
+//
+//  BNRStockPortfolio.m
+//  Stocks
+//
+//  Created by idris on 4/10/15.
+//  Copyright (c) 2015 Idris Raja. All rights reserved.
+//
+
+#import "BNRStockPortfolio.h"
+
+@implementation BNRStockPortfolio
+
+@end
