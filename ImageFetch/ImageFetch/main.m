2caabf754b423e20ef54f954d5763d1bdfa95d0c
Idris Raja <idris.raja@gmail.com>
2015-04-11 19:17:22 +0700

    An example of fetching an image from the web and then saving the image to file. Two things can go wrong. Note the NSDataWritingAtomia option which says to either write the whole file, or write nothing

---
+++ ImageFetch/ImageFetch/main.m
@@ -0,0 +1,37 @@
+//
+//  main.m
+//  ImageFetch
+//
+//  Created by idris on 4/11/15.
+//  Copyright (c) 2015 Idris Raja. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSURL *url = [NSURL URLWithString: @"http://www.google.com/images/logos/ps_logo2.png"];
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        NSError *error = nil;
+        NSData *data = [NSURLConnection sendSynchronousRequest:request
+                                             returningResponse:NULL
+                                                         error:&error];
+
+        if(!data){
+            NSLog(@"fetch failed: %@", [error  localizedDescription]);
+            return 1;
+        }
+
+        NSLog(@"The file is %lu byes", (unsigned long)[data length]);
+
+        BOOL written = [data writeToFile:@"/tmp/google.png"
+                                 options:NSDataWritingAtomic
+                                   error:&error];
+        if (!written){
+            NSLog(@"write failed: %@", [error localizedDescription]);
+            return 1;
+        }
+        NSLog(@"Success!");
+    }
+    return 0;
+}
