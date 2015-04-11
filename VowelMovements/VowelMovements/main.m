a01a8730fa5c54245b8ceb13146f0f9252e9ce7a
Idris Raja <idris.raja@gmail.com>
2015-04-11 22:54:16 +0700

    create blocks, which is similar to a lambda or an anonymous function

---
+++ VowelMovements/VowelMovements/main.m
@@ -0,0 +1,40 @@
+//
+//  main.m
+//  VowelMovements
+//
+//  Created by idris on 4/11/15.
+//  Copyright (c) 2015 Idris Raja. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Create array of strings and a contained for devowelized ones
+        NSArray *originalStrings = @[@"Sauerkraut", @"Raygun", @"Big Nerd Ranch", @"Mississippi"];
+        NSLog(@"original strings : %@", originalStrings);
+        NSMutableArray *devowelizedStrings = [NSMutableArray array];
+
+        // create a list of characters to be removed from the string
+        NSArray *vowels = @[@"a", @"e", @"i", @"o", @"u"];
+
+        // declare the block variable
+        void (^devowelizer)(id, NSUInteger, BOOL *);
+        devowelizer = ^(id string, NSUInteger i, BOOL *stop){
+            NSMutableString *newString = [NSMutableString stringWithString:string];
+
+            // iterate over the array of vowels, replacing occurences of each
+            // with an empty string
+
+            for (NSString *s in vowels){
+                NSRange fullRange = NSMakeRange(0, [newString length]);
+                [newString replaceOccurrencesOfString:s
+                                           withString:@""
+                                              options:NSCaseInsensitiveSearch
+                                                range:fullRange];
+            }
+            [devowelizedStrings addObject:newString];
+        }; // End of block assignment
+    }
+    return 0;
+}
