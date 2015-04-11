ab88ce681c365f710d491e8bd9b4d78309f0567d
Idris Raja <idris.raja@gmail.com>
2015-04-11 17:55:46 +0700

    playing with NSError

--- stringz/stringz/main.m
+++ stringz/stringz/main.m
@@ -14,11 +14,21 @@ int main(int argc, const char * argv[]) {
         for (int i = 0; i < 100; i++){
             [str appendString:@"Idris is cool!\n"];
         }
-        [str writeToFile:@"/tmp/cool.txt"
-              atomically:YES
-                encoding:NSUTF8StringEncoding
-                   error:NULL];
-        NSLog(@"done writing /tmp/cool.txt");
+
+        // Declare a pointer to an NSError objet, but do not instantiate it
+        // The NSError instance will only be created if there is, in fact, an error
+        NSError *error;
+
+        // Pass the NSError pointer by reference to the NSString method
+        BOOL success = [str writeToFile:@"/menoexist/cool.txt"
+                             atomically:YES
+                               encoding:NSUTF8StringEncoding
+                                  error:&error];
+        if (success){
+            NSLog(@"done writing /tmp/cool.txt");
+        } else {
+            NSLog(@"writing /tmp/cool.txt failed: %@", [error localizedDescription]);
+        }
     }
     return 0;
 }
